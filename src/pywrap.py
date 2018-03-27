8339d109836ab94343148518f7b2c2dbca946bb8
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-03-26 22:26:50 -0400

    nightly

--- src/pywrap.py
+++ src/pywrap.py
@@ -61,7 +61,7 @@ def predict(counts,trees,prediction_mode="branching",drop_mode="zeros",processor
 
     predict_file("./working/counts.txt",trees,prediction_mode=prediction_mode,drop_mode=drop_mode,processors=processors,output_location=output_location,features=features,reporting=reporting)
 
-    return np.loadtxt(output_location+".prediction")
+    return np.loadtxt(output_location+".0.prediction")
 
 
 def fit_predict_file(counts,drop_mode="zeros",prediction_mode="branching",trees="1",leaves="1",in_features="1",out_features="1",feature_subsample="1",sample_subsample="1",processors="1",output_location="./working/temp",features=None,samples=None,reporting=None):
