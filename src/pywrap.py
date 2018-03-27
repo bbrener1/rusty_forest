fa641ecbb5d7d90ec6ef6ae6807cd8f186ea02f8
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-03-26 21:11:19 -0400

    nightly

--- src/pywrap.py
+++ src/pywrap.py
@@ -8,7 +8,7 @@ import re
 def main():
     print "Testing"
     prefix = os.getcwd()
-    fit_predict_file(counts=prefix+"/testing/iris.drop",drop_mode="zeros",prediction_mode="branching",trees="1",leaves="10",in_features="4",out_features="4",feature_subsample="4",sample_subsample="150",processors="1",output_location=prefix+"/testing/wrapper_test/iris",features=prefix+"/testing/iris.features")
+    fit_predict_file(counts=prefix+"/testing/iris.drop",drop_mode="zeros",prediction_mode="branching",trees="1",leaves="10",in_features="4",out_features="4",feature_subsample="4",sample_subsample="150",processors="1",output_location=prefix+"/testing/precomputed_trees/iris",features=prefix+"/testing/iris.features")
 
     fit_predict_file(prefix+"/testing/simple.txt",sample_subsample="8",output_location=prefix+"/testing/precomputed_trees/simple")
 
@@ -30,7 +30,7 @@ def fit_predict(counts,drop_mode="zeros",prediction_mode="branching",trees="1",l
 
     tree_files = fit(counts,drop_mode=drop_mode,prediction_mode=prediction_mode,trees=trees,leaves=leaves,in_features=in_features,out_features=out_features,feature_subsample=feature_subsample,sample_subsample=sample_subsample,processors="10",output_location=output_location,features=features,samples=samples)
 
-    predict(counts,)
+    return predict(counts,)
 
 def fit(counts,drop_mode="zeros",prediction_mode="branching",trees="1",leaves="1",in_features="1",out_features="1",feature_subsample="1",sample_subsample="1",processors="1",output_location="./working/temp",features=None,samples=None,reporting=None):
 
