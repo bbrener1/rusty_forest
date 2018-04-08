e61663314b93a6f505cdca476cb6d22524b7edab
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-04-07 22:02:27 -0400

    nightly

--- src/rank_table.rs
+++ src/rank_table.rs
@@ -70,7 +70,7 @@ impl RankTable {
     }
 
     pub fn sort_by_feature(& self, feature: &str) -> Vec<usize> {
-        self.meta_vector[self.feature_dictionary[feature]].give_dropped_order()
+        self.meta_vector[self.feature_dictionary[feature]].give_draw_order()
     }
 
     pub fn split_indecies_by_feature(&self, feature: &str, split: &f64) -> (Vec<usize>,Vec<usize>){
