b06bc0722c98c3c652fb02cd290f3547f6aa7c10
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-04-15 22:16:07 -0400

    nightly

--- src/boosted_forest.rs
+++ src/boosted_forest.rs
@@ -345,7 +345,7 @@ pub fn weighted_sampling<T: Clone>(draws: usize, samples: &Vec<T>, weights: &Vec
     else {
 
         let mut local_weights: Vec<(usize,f64)> = weights.iter().cloned().enumerate().collect();
-        println!("weight debug: {}", local_weights.len());
+        // println!("weight debug: {}", local_weights.len());
         let mut maximum_weight = local_weights.iter().max_by(|a,b| a.partial_cmp(&b).unwrap_or(Ordering::Greater)).map(|x| x.clone()).unwrap_or((0,0.));
 
         for i in 0..draws {
@@ -362,7 +362,7 @@ pub fn weighted_sampling<T: Clone>(draws: usize, samples: &Vec<T>, weights: &Vec
             let mut current_draw = local_weights[random_index];
 
             while accumulator <= maximum_weight.1 {
-                println!("{}",local_weights.len());
+                // println!("{}",local_weights.len());
                 accumulator += current_draw.1;
                 random_index = rng.gen_range::<usize>(0,local_weights.len());
                 current_draw = local_weights[random_index];
