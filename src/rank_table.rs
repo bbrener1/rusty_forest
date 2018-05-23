25a946e8aef519656fe6b92a0d711326cf9620b5
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-05-23 00:48:42 -0400

    nightly

--- src/rank_table.rs
+++ src/rank_table.rs
@@ -339,7 +339,9 @@ impl RankTable {
                 NormMode::L2 => l2_maximum(&disp_mtx, feature_weights.unwrap_or(&vec![1.;self.feature_names.len()])),
             };
 
+            println!("{:?}", maximum);
             maximum.map(|z| (z.0, z.1 * draw_order.len() as f64));
+            println!("{:?}", maximum);
 
             maximum
 
