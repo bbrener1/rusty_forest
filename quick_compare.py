e70bf1987716146eab53992298ab74e82e56e87c
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-07-05 20:58:55 -0400

    nightly

--- quick_compare.py
+++ quick_compare.py
@@ -16,7 +16,6 @@ print "Mean Feature Correlation:"
 correlations = []
 for i in range(truth.T.shape[0]):
     correlations.append(pearsonr(truth.T[i],pred.T[i])[0])
-print len(correlations)
 print np.mean(correlations)
 
 print "Mean Absolute Error"
